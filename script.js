c2f8b15fd5a6b11dc136f62bbaa224700fc093bc
maxiroots-star <maxiroots@googlemail.com>
2026-06-15 18:17:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const trackA = document.getElementById("trackA");
 const trackB = document.getElementById("trackB");
 
@@ -7,29 +6,25 @@ const playB = document.getElementById("playB");
 
 const crossfader = document.getElementById("crossfader");
 
-/* START AUDIO ON FIRST TAP (mobile fix) */
+/* MOBILE AUDIO UNLOCK */
 document.body.addEventListener("click", () => {
     trackA.volume = 0.5;
     trackB.volume = 0.5;
 }, { once: true });
 
 /* PLAY BUTTONS */
-playA.addEventListener("click", () => {
+playA.onclick = () => {
     trackA.play();
-});
+};
 
-playB.addEventListener("click", () => {
+playB.onclick = () => {
     trackB.play();
-});
+};
 
-/* CROSSFADER LOGIC */
-crossfader.addEventListener("input", () => {
+/* CROSSFADER */
+crossfader.oninput = () => {
+    let v = crossfader.value / 100;
 
-    let value = crossfader.value; // 0 - 100
-
-    let volumeA = 1 - (value / 100);
-    let volumeB = value / 100;
-
-    trackA.volume = volumeA;
-    trackB.volume = volumeB;
-});
+    trackA.volume = 1 - v;
+    trackB.volume = v;
+};
